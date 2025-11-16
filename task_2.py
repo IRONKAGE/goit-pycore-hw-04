5ed2d24b6bf5faaf08ab849be68978e54bb45e70
IRONKAGE 👻 Oleh Hatsenko <IRONKAGE@gmail.com>
2025-11-16 20:49:23 +0200

    fix random numbers between numbers

--- task_2.py
+++ task_2.py
@@ -1,12 +1,17 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if not (1 <= min <= max <= 1000 and min <= quantity <= max):
+    if not (1 <= min <= 1000 and 
+            1 <= max <= 1000 and 
+            min < max and
+            quantity > 0 and 
+            quantity <= (max - min + 1)):
         return []
-
-    numbers = random.sample(range(min, max + 1), quantity)
-    numbers.sort()
-    return numbers
+    range_of_numbers = list(range(min, max + 1))
+    selected_numbers = random.sample(range_of_numbers, quantity)
+    selected_numbers.sort()
+        
+    return selected_numbers
 
 # Приклад використання
 # Випадкові числа для лотерейного квитка від 1 до 49 (6 чисел)
@@ -20,3 +25,7 @@ print("Екзаменаійні питання:", exam_paper)
 # Приклад з невалідними даними
 invalid_lottery = get_numbers_ticket(1, 10, 15)
 print("Невалідний запит (повинен повернути порожній список):", invalid_lottery)
+
+# Приклад з числами, які не починаються з 1
+mid_five_random_number = get_numbers_ticket(10, 20, 5)
+print("П'ять випадкових чисел від 10 до 20:", mid_five_random_number)
